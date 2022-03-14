d251e35e5d14ba290dc2d73ab8ec0c4c105fe92c
monsonjeremy <monson.jeremy@gmail.com>
2022-03-14 14:22:52 -0700

    fix: move check entitlement logic to backend

--- packages/axyz-js/src/actions/checkEntitlement.ts
+++ packages/axyz-js/src/actions/checkEntitlement.ts
@@ -1,31 +1,35 @@
 import { AxiosInstance } from 'axios';
-import checkTokenBalances from '../utils/checkTokenBalances';
+import validateEntitlement from '../api/validateEntitlement';
+
 import Context from '../utils/context';
 
 // eslint-disable-next-line import/prefer-default-export
 export const CreateCheckEntitlement = (api: AxiosInstance, context: Context) => {
-  const checkEntitlement = async (entitlementName: string) => {
-    const entitlements = context.get('entitlements');
+  const checkEntitlement = async (entitlementKey: string) => {
+    const publicKey = context.get('publicKey');
 
-    if (!entitlementName) {
+    if (!publicKey) {
+      throw new Error('Public key not found. Please ensure a wallet is connected.');
+    }
+
+    if (!entitlementKey) {
       throw new Error('Entitlement is required.');
     }
 
-    if (!entitlements) {
-      throw new Error(
-        'Entitlements map not found. Ensure you have called `getEntitlements` first.'
-      );
+    try {
+      const validation = await validateEntitlement(api, context, entitlementKey);
+      return validation;
+    } catch (error) {
+      return {
+        errors: ['Could not validate entitlement.'],
+        chain: 'SOL',
+        entitlement: entitlementKey,
+        isEntitledByMint: false,
+        isEntitledByNFT: false,
+        matchedNFTs: [],
+        isEntitled: false,
+      };
     }
-
-    const mintsForEntitlement = entitlements[entitlementName];
-
-    if (!mintsForEntitlement) {
-      throw new Error(`Entitlement ${entitlementName} not found.`);
-    }
-
-    const isEntitled = checkTokenBalances(context, mintsForEntitlement);
-
-    return { entitlement: entitlementName, isEntitled };
   };
   return checkEntitlement;
 };
