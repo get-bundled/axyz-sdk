151a0d393651bfe5afdfa72ba6d10a9e4e1379f2
monsonjeremy <monson.jeremy@gmail.com>
2022-04-10 20:29:30 -0700

    fix: getbundled to getaxyz

--- packages/axyz-js/src/constants/url.ts
+++ packages/axyz-js/src/constants/url.ts
@@ -1,8 +1,8 @@
 import { DEVELOPMENT, LOCAL, PRODUCTION } from './environments';
 
 export const LOCAL_URL = 'http://localhost:3333';
-export const DEVELOPMENT_URL = 'https://api-dev.getbundled.co';
-export const PRODUCTION_URL = 'https://api.getbundled.co';
+export const DEVELOPMENT_URL = 'https://api-dev.getaxyz.com';
+export const PRODUCTION_URL = 'https://api.getaxyz.com';
 
 export const AxyzAPIUrls = {
   [LOCAL]: LOCAL_URL,
