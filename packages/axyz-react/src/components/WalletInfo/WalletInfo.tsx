bca50d307fb25a4921f487cd757a4626fd3dc636
monsonjeremy <monson.jeremy@gmail.com>
2022-05-11 18:46:15 -0700

    fix: incorrect dark theme colors

--- packages/axyz-react/src/components/WalletInfo/WalletInfo.tsx
+++ packages/axyz-react/src/components/WalletInfo/WalletInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { styled, Text, Button } from '@nextui-org/react';
+import { styled, Text, Button, useTheme } from '@nextui-org/react';
 import { Box } from '../Blocks';
 
 interface Props {
@@ -27,51 +27,55 @@ const FlexColumn = styled('div', {
   alignItems: 'flex-start',
 });
 
-const WalletInfo: React.FC<Props> = ({ address, name, Icon, setDisconnect }) => (
-  <Root>
-    <Box css={{ display: 'flex', flexFlow: 'row' }}>
-      <div style={{ height: 40, marginRight: 10 }}>{Icon}</div>
-      <FlexColumn>
-        <Text
-          css={{
-            color: '#0A0B0D',
-            fontWeight: 700,
-            fontSize: 16,
-            lineHeight: '16px',
-            marginBottom: 4,
-          }}
-        >
-          {name}
-        </Text>
-        <Text
-          css={{
-            color: '#5C616D',
-            fontWeight: 700,
-            fontSize: 12,
-            lineHeight: '12px',
-          }}
-        >
-          {address &&
-            `${address.slice(0, 4)}...${address.slice(address.length - 4, address.length)}`}
-        </Text>
-      </FlexColumn>
-    </Box>
-    <Button
-      light
-      css={{
-        minWidth: 'unset',
-        padding: 8,
-        display: 'flex',
-        fontSize: 12,
-        fontWeight: 700,
-        lineHeight: '12px',
-        color: '#8D8CFB',
-      }}
-      onClick={setDisconnect}
-    >
-      Disconnect
-    </Button>
-  </Root>
-);
+const WalletInfo: React.FC<Props> = ({ address, name, Icon, setDisconnect }) => {
+  const { isDark } = useTheme();
+
+  return (
+    <Root>
+      <Box css={{ display: 'flex', flexFlow: 'row' }}>
+        <div style={{ height: 40, marginRight: 10 }}>{Icon}</div>
+        <FlexColumn>
+          <Text
+            css={{
+              color: isDark ? '#fff' : '#0A0B0D',
+              fontWeight: 700,
+              fontSize: 16,
+              lineHeight: '16px',
+              marginBottom: 4,
+            }}
+          >
+            {name}
+          </Text>
+          <Text
+            css={{
+              color: '#5C616D',
+              fontWeight: 700,
+              fontSize: 12,
+              lineHeight: '12px',
+            }}
+          >
+            {address &&
+              `${address.slice(0, 4)}...${address.slice(address.length - 4, address.length)}`}
+          </Text>
+        </FlexColumn>
+      </Box>
+      <Button
+        light
+        css={{
+          minWidth: 'unset',
+          padding: 8,
+          display: 'flex',
+          fontSize: 12,
+          fontWeight: 700,
+          lineHeight: '12px',
+          color: '#8D8CFB',
+        }}
+        onClick={setDisconnect}
+      >
+        Disconnect
+      </Button>
+    </Root>
+  );
+};
 
 export default WalletInfo;
--- packages/axyz-react/src/components/WalletStatusControl/WalletDisconnectOverlay.tsx
+++ packages/axyz-react/src/components/WalletStatusControl/WalletDisconnectOverlay.tsx
@@ -1,4 +1,4 @@
-import { Button, Text } from '@nextui-org/react';
+import { Button, Text, useTheme } from '@nextui-org/react';
 import React from 'react';
 import { Box } from '../Blocks';
 
@@ -7,82 +7,85 @@ interface Props {
   disconnect: () => Promise<void>;
 }
 
-const WalletDisconnectOverlay: React.FC<Props> = ({ cancel, disconnect }) => (
-  <Box
-    css={{
-      borderRadius: '$lg',
-      background: '$background',
-      position: 'absolute',
-      top: '0px',
-      left: '0px',
-      right: '0px',
-      bottom: '0px',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      zIndex: 210,
-    }}
-  >
-    <Text
-      css={{
-        fontWeight: 700,
-        fontSize: 16,
-        lineHeight: '16px',
-        textAlign: 'center',
-        color: '#0A0B0D',
-        marginBottom: '30px',
-        width: 199,
-        height: 35,
-      }}
-    >
-      Are you sure you want to disconnect?
-    </Text>
+const WalletDisconnectOverlay: React.FC<Props> = ({ cancel, disconnect }) => {
+  const { isDark } = useTheme();
+  return (
     <Box
       css={{
+        borderRadius: '$lg',
+        background: '$background',
+        position: 'absolute',
+        top: '0px',
+        left: '0px',
+        right: '0px',
+        bottom: '0px',
         display: 'flex',
-        justifyContent: 'space-between',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 210,
       }}
     >
-      <Button
-        bordered
-        rounded
-        onClick={cancel}
+      <Text
         css={{
-          minWidth: 64,
-          width: 64,
-          height: 28,
           fontWeight: 700,
-          fontSize: 12,
-          lineHeight: '12px',
-          border: '1px solid #ECF0F8',
-          background: 'transparent',
-          marginRight: '10px',
-          color: '#5C616D',
+          fontSize: 16,
+          lineHeight: '16px',
+          textAlign: 'center',
+          color: isDark ? '#fff' : '#0A0B0D',
+          marginBottom: '30px',
+          width: 199,
+          height: 35,
         }}
       >
-        cancel
-      </Button>
-      <Button
+        Are you sure you want to disconnect?
+      </Text>
+      <Box
         css={{
-          minWidth: 116,
-          width: 116,
-          height: 28,
-          fontWeight: 700,
-          fontSize: 12,
-          lineHeight: '12px',
-          border: '1px solid #ECF0F8',
-          background: 'transparent',
-          color: '#8D8CFB',
+          display: 'flex',
+          justifyContent: 'space-between',
         }}
-        bordered
-        rounded
-        onClick={disconnect}
       >
-        Yes, disconnect.
-      </Button>
+        <Button
+          bordered
+          rounded
+          onClick={cancel}
+          css={{
+            minWidth: 64,
+            width: 64,
+            height: 28,
+            fontWeight: 700,
+            fontSize: 12,
+            lineHeight: '12px',
+            border: '1px solid #ECF0F8',
+            background: 'transparent',
+            marginRight: '10px',
+            color: isDark ? '#EEF0F3' : '#5C616D',
+          }}
+        >
+          cancel
+        </Button>
+        <Button
+          css={{
+            minWidth: 116,
+            width: 116,
+            height: 28,
+            fontWeight: 700,
+            fontSize: 12,
+            lineHeight: '12px',
+            border: '1px solid #ECF0F8',
+            background: 'transparent',
+            color: '#8D8CFB',
+          }}
+          bordered
+          rounded
+          onClick={disconnect}
+        >
+          Yes, disconnect.
+        </Button>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default WalletDisconnectOverlay;
